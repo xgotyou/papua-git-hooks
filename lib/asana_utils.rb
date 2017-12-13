c6adcf6996c204d59cd3625738ad4248e158519f
Alexey Barkov <xgotyou@gmail.com>
2017-12-13 22:58:04 +0300

    Try to mock asana

--- lib/asana_utils.rb
+++ lib/asana_utils.rb
@@ -1,8 +1,23 @@
+require 'asana'
+
 module Papua
   class AsanaUtils
     def self.find_task(id)
       raise "Env variable asana_token should be set" unless ENV['asana_token']
       { id: id }
+    end
+
+    def self.open_task(id)
+      client = Asana::Client.new do |c|
+        c.authentication :access_token, '0/575a63469f9f126097f3996e0c0c5f38'
+      end
+
+      begin
+        task = client.tasks.find_by_id(id)
+      rescue
+        puts "Unable to find task with id (#{feature_name}). Please check. Feature name should be Asana task id."
+        exit 1
+      end
     end
   end
 end
--- spec/asana_utils_spec.rb
+++ spec/asana_utils_spec.rb
@@ -1,9 +1,11 @@
 require 'minitest/autorun'
+require 'asana'
 require 'asana_utils'
 
 describe 'AsanaUtile' do
-  before { ENV['asana_token'] = '0/575a63469f9f126097f3996e0c0c5f38' }
+  before { ENV['asana_token'] = token }
 
+  let :token { '0/575a63469f9f1' }
   let :task_id { '112233445556' }
 
   it "should get task by id" do
@@ -18,10 +20,16 @@ describe 'AsanaUtile' do
   end
 
   it "should move task to specified state" do
-    skip 'not implemented'
+    asana_client = Minitest::Mock.new
+    asana_client.expect :authentication, nil, [:access_token, token]
+    asana_client.expect :tasks, tasks = Minitest::Mock.new
+    tasks.expect :find_by_id, {}, [task_id]
+
+    Asana::Client.stub :new, asana_client do
+      Papua::AsanaUtils.open_task(task_id)
+    end
+
+    assert_mock asana_client
   end
 
-  it "should throw exception if transition not allowed" do
-    skip 'not implemented'
-  end
 end
